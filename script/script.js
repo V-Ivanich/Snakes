f4d4af346c52384db5bdd8adb2e6061104868a81
V-Ivanich <chipai_dom@mail.ru>
2022-03-13 20:28:31 +0300

    code plus

--- script/script.js
+++ script/script.js
@@ -1,4 +1,20 @@
 
 const hedSnake = document.querySelector('.heads');
 const pole = document.querySelector('.container');
+const btn = document.querySelector('.btnStart');
+let pos = 0;
 
+btn.addEventListener('click', () => {
+  let time = setInterval(frame, 20);
+  frame();
+})
+
+function frame() {
+  if (pos == 380) {
+    clearInterval(time);
+  } else {
+    pos++;
+    hedSnake.style.top = pos + 'px';
+    hedSnake.style.left = pos + 'px';
+  }
+}
