f44fd6b2312a1ddfe08c805752fb77a47e77045f
V-Ivanich <chipai_dom@mail.ru>
2022-03-13 21:29:27 +0300

    move head

--- script/script.js
+++ script/script.js
@@ -1,20 +1,37 @@
 
-const hedSnake = document.querySelector('.heads');
-const pole = document.querySelector('.container');
-const btn = document.querySelector('.btnStart');
-let pos = 0;
+const hedSnake = document.querySelector('.heads'),
+  pole = document.querySelector('.container'),
+  btn = document.querySelector('.btnStart'),
+  btnSt = document.querySelector('.btnStop'),
+  hori = document.querySelector('.horizont'),
+  vert = document.querySelector('.vertikal');
+
+let time,
+  posH = 0,
+  posV = 0;
 
 btn.addEventListener('click', () => {
-  let time = setInterval(frame, 20);
+  time = setInterval(frame, 20);
   frame();
 })
 
 function frame() {
-  if (pos == 380) {
-    clearInterval(time);
+  if (posH == 400) {
+    posH = -20;
   } else {
-    pos++;
-    hedSnake.style.top = pos + 'px';
-    hedSnake.style.left = pos + 'px';
+    posH++;
+    hedSnake.style.top = posH + 'px';
+    // hedSnake.style.left = pos + 'px';
   }
 }
+btnSt.addEventListener('click', () => {
+  clearInterval(time);
+})
+hori.addEventListener('click', () => {
+  if (posV == 400) {
+    posV = -20;
+  } else {
+    posV++;
+    hedSnake.style.left = posV + 'px';
+  }
+})
