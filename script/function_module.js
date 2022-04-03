b8170feb1ec4cc58a00dc6c1c722d8e8652f75c8
V-Ivanich <chipai_dom@mail.ru>
2022-04-03 11:13:52 +0300

    fix module js

---
+++ script/function_module.js
@@ -0,0 +1,124 @@
+
+//? вывод результата и его сравнение
+function results() {
+  scoreGame.push([inPut.value, legthSnake, gameScore]);
+  let temp = []; //* промежуточный массив
+  let maxPoint = 0; //* максимальное значение
+  while (scoreGame.length != 0) {
+    for (let i = 0; i < scoreGame.length - 1; i++) {
+      if (scoreGame[maxPoint][2] < scoreGame[i + 1][2])
+        maxPoint = i + 1;
+    }
+    temp.push(scoreGame[maxPoint]);
+    scoreGame.splice(maxPoint, 1);
+    maxPoint = 0;
+  }
+  scoreGame = [];
+  scoreGame = temp.slice(0, 7);
+
+  for (let i = 0; i < scoreGame.length; i++) {
+    records.rows[i + 1].cells[0].innerHTML = scoreGame[i][0];
+    records.rows[i + 1].cells[1].innerHTML = scoreGame[i][1];
+    records.rows[i + 1].cells[2].innerHTML = scoreGame[i][2];
+  }
+}
+
+//! рождение и рост змеи
+function bodySnake() {
+  if (legthSnake > posMass.length) posMass.push([0, 0]);
+  let moveData = [];
+  for (let i = posMass.length - 1; i > 0; i--) {
+    moveData[i] = posMass[i - 1];
+    posMass[i] = moveData[i];
+  }
+  posMass[0] = [posRow, posCol];
+  let x, y;
+  for (let n = posMass.length - 1; n >= 0; n--) {
+    y = posMass[n][0];
+    x = posMass[n][1];
+    hedSnake = massiv[y][x];
+    hedSnake.classList.add('kub_activ');
+  }
+}
+
+//? фун-я направления движения,
+//? в зависимости от нажатой клавиши
+function frame() {
+  switch (flag) {
+    case 1:
+      if (posCol == 19) posCol = -1;
+      resetClass(1, 0, 1);
+      break;
+    case 2:
+      if (posRow == 19) posRow = -1;
+      resetClass(1, 1, 0);
+      break;
+    case 3:
+      if (posCol == 0) posCol = 20;
+      resetClass(0, 0, 1);
+      break;
+    case 4:
+      if (posRow == 0) posRow = 20;
+      resetClass(0, 1, 0);
+      break;
+  }
+}
+
+function foods() {
+  let m = randOm(0, 19);
+  let n = randOm(0, 19);
+
+  if (scanBody(m, n)) {
+    foods();
+  } else {
+    foo = massiv[m][n];
+    foo.classList.add('food');
+    forSnake = [m, n];
+  }
+}
+
+function switchLanguage() {
+  document.querySelector('.titleH2').innerHTML = lang['title'][ru_us];
+  document.querySelector('.lenSnake').innerHTML = lang['length'][ru_us];
+  document.querySelector('.resUlt').innerHTML = lang['result'][ru_us];
+  document.querySelector('#information').innerHTML = lang['info'][ru_us];
+  document.querySelector('.btnStart').innerHTML = lang['btStart'][ru_us];
+  document.querySelector('.btnCont').innerHTML = lang['continue'][ru_us];
+  document.querySelector('.btnStop').innerHTML = lang['stop'][ru_us];
+  document.querySelector('.gamOv').innerHTML = lang['over'][ru_us];
+  document.querySelector('.rec').innerHTML = lang['records'][ru_us];
+  document.querySelector('.namTab').innerHTML = lang['tablName'][ru_us];
+  document.querySelector('.lenTab').innerHTML = lang['tablLen'][ru_us];
+  document.querySelector('.scTab').innerHTML = lang['tablScore'][ru_us];
+  document.querySelector('.infoTit').innerHTML = lang['titlInfo'][ru_us];
+  document.querySelector('.paragraf').innerHTML = lang['textInfo'][ru_us];
+}
+
+//? проверка на положение тела змеи
+function scanBody(y, x) {
+  let result = false,
+    z;
+  for (let i = 0; i < posMass.length; i++) {
+    z = posMass[i][0];
+    if (y == z) {
+      z = posMass[i][1];
+      if (x == z) {
+        result = true;
+        break;
+      } else continue;
+    } else continue;
+  }
+  return result;
+}
+
+function resazeWindow() {
+  if (widthWindow <= 425) {
+    mobBtn.classList.remove('noVisible');
+    leftBlock.classList.add('absolut');
+    rightBlock.classList.add('absolut');
+  } else {
+    mobBtn.classList.add('noVisible');
+    leftBlock.classList.remove('absolut');
+    rightBlock.classList.remove('absolut');
+  }
+}
--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 
+
 const pole = document.querySelector('.container'),
   btn = document.querySelector('.btnStart'),
   btnSt = document.querySelector('.btnStop'),
@@ -58,22 +59,6 @@ function start() {
   resazeWindow();
 }
 
-function switchLanguage() {
-  document.querySelector('.titleH2').innerHTML = lang['title'][ru_us];
-  document.querySelector('.lenSnake').innerHTML = lang['length'][ru_us];
-  document.querySelector('.resUlt').innerHTML = lang['result'][ru_us];
-  document.querySelector('#information').innerHTML = lang['info'][ru_us];
-  document.querySelector('.btnStart').innerHTML = lang['btStart'][ru_us];
-  document.querySelector('.btnCont').innerHTML = lang['continue'][ru_us];
-  document.querySelector('.btnStop').innerHTML = lang['stop'][ru_us];
-  document.querySelector('.gamOv').innerHTML = lang['over'][ru_us];
-  document.querySelector('.rec').innerHTML = lang['records'][ru_us];
-  document.querySelector('.namTab').innerHTML = lang['tablName'][ru_us];
-  document.querySelector('.lenTab').innerHTML = lang['tablLen'][ru_us];
-  document.querySelector('.scTab').innerHTML = lang['tablScore'][ru_us];
-  document.querySelector('.infoTit').innerHTML = lang['titlInfo'][ru_us];
-  document.querySelector('.paragraf').innerHTML = lang['textInfo'][ru_us];
-}
 
 windOws.classList.add('wind_info');
 
@@ -125,37 +110,6 @@ info.addEventListener('click', () => {
   windOws.style.left = posX - 250 + 'px';
 });
 
-//! рождение и рост змеи
-function bodySnake() {
-  if (legthSnake > posMass.length) posMass.push([0, 0]);
-  let moveData = [];
-  for (let i = posMass.length - 1; i > 0; i--) {
-    moveData[i] = posMass[i - 1];
-    posMass[i] = moveData[i];
-  }
-  posMass[0] = [posRow, posCol];
-  let x, y;
-  for (let n = posMass.length - 1; n >= 0; n--) {
-    y = posMass[n][0];
-    x = posMass[n][1];
-    hedSnake = massiv[y][x];
-    hedSnake.classList.add('kub_activ');
-  }
-}
-
-
-function foods() {
-  let m = randOm(0, 19);
-  let n = randOm(0, 19);
-
-  if (scanBody(m, n)) {
-    foods();
-  } else {
-    foo = massiv[m][n];
-    foo.classList.add('food');
-    forSnake = [m, n];
-  }
-}
 
 btn.addEventListener('click', () => { //? старт
   gameOver.classList.remove('over_active');
@@ -180,28 +134,6 @@ btnSt.addEventListener('click', () => { //? стоп
   clearInterval(time);
 })
 
-//? фун-я направления движения,
-//? в зависимости от нажатой клавиши
-function frame() {
-  switch (flag) {
-    case 1:
-      if (posCol == 19) posCol = -1;
-      resetClass(1, 0, 1);
-      break;
-    case 2:
-      if (posRow == 19) posRow = -1;
-      resetClass(1, 1, 0);
-      break;
-    case 3:
-      if (posCol == 0) posCol = 20;
-      resetClass(0, 0, 1);
-      break;
-    case 4:
-      if (posRow == 0) posRow = 20;
-      resetClass(0, 1, 0);
-      break;
-  }
-}
 
 //? смена класса у div-ов
 function resetClass(flagSet, pos_1, pos_2) {
@@ -265,55 +197,3 @@ document.onkeydown = function (event) {
   }
 }
 
-//? проверка на положение тела змеи
-function scanBody(y, x) {
-  let result = false,
-    z;
-  for (let i = 0; i < posMass.length; i++) {
-    z = posMass[i][0];
-    if (y == z) {
-      z = posMass[i][1];
-      if (x == z) {
-        result = true;
-        break;
-      } else continue;
-    } else continue;
-  }
-  return result;
-}
-
-//? вывод результата и его сравнение
-function results() {
-  scoreGame.push([inPut.value, legthSnake, gameScore]);
-  let temp = []; //* промежуточный массив
-  let maxPoint = 0; //* максимальное значение
-  while (scoreGame.length != 0) {
-    for (let i = 0; i < scoreGame.length - 1; i++) {
-      if (scoreGame[maxPoint][2] < scoreGame[i + 1][2])
-        maxPoint = i + 1;
-    }
-    temp.push(scoreGame[maxPoint]);
-    scoreGame.splice(maxPoint, 1);
-    maxPoint = 0;
-  }
-  scoreGame = [];
-  scoreGame = temp.slice(0, 7);
-
-  for (let i = 0; i < scoreGame.length; i++) {
-    records.rows[i + 1].cells[0].innerHTML = scoreGame[i][0];
-    records.rows[i + 1].cells[1].innerHTML = scoreGame[i][1];
-    records.rows[i + 1].cells[2].innerHTML = scoreGame[i][2];
-  }
-}
-
-function resazeWindow() {
-  if (widthWindow <= 425) {
-    mobBtn.classList.remove('noVisible');
-    leftBlock.classList.add('absolut');
-    rightBlock.classList.add('absolut');
-  } else {
-    mobBtn.classList.add('noVisible');
-    leftBlock.classList.remove('absolut');
-    rightBlock.classList.remove('absolut');
-  }
-}
